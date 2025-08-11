88b29e6308a90a21d8c4dd48e3bcd553ff2a913a
PraveshKasliwal <praveshkasliwal123@gmail.com>
2025-08-11 17:36:00 +0530

    env update

--- backend/utils/s3Uploader.js
+++ backend/utils/s3Uploader.js
@@ -4,8 +4,8 @@ const multerS3 = require('multer-s3');
 
 // Configure AWS
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 console.log('here');
